70c9391cc0ab38d08a2c91ad24a519e3f5a44702
Heidi de Villiers <hdv@hetzner.co.za>
2015-11-20 13:36:07 +0000

    HW3 Part 1 and Part 2 completed

--- rottenpotatoes/features/step_definitions/movie_steps.rb
+++ rottenpotatoes/features/step_definitions/movie_steps.rb
@@ -2,10 +2,9 @@
 
 Given /the following movies exist/ do |movies_table|
   movies_table.hashes.each do |movie|
-    # each returned element will be a hash whose key is the table header.
-    # you should arrange to add that movie to the database here.
+    Movie.create movie
+    # Movie.create(:title => movie.title, :rating => movie.rating, :release_date => movie.release_date)
   end
-  fail "Unimplemented"
 end
 
 # Make sure that one string (regexp) occurs before or after another one
@@ -21,14 +20,28 @@ end
 #  "When I uncheck the following ratings: PG, G, R"
 #  "When I check the following ratings: G"
 
-When /I (un)?check the following ratings: (.*)/ do |uncheck, rating_list|
+Given /I (un)?check the following ratings: (.*)/ do |uncheck, rating_list|
   # HINT: use String#split to split up the rating_list, then
   #   iterate over the ratings and reuse the "When I check..." or
   #   "When I uncheck..." steps in lines 89-95 of web_steps.rb
-  fail "Unimplemented"
+  rating_list.split(',').each do |rating|
+    if uncheck
+      uncheck "ratings_#{rating}"
+    else
+      check "ratings_#{rating}"
+    end
+  end
 end
 
 Then /I should see all the movies/ do
   # Make sure that all the movies in the app are visible in the table
-  fail "Unimplemented"
+  movies = Movie.all
+  
+  if movies.size == 10
+    movies.each do |movie|
+      fail"#{movie.title} did not appear" unless page.body =~ /#{movie.title}/m
+    end
+    else
+      fail "Number of movies incorrect"
+    end
 end
